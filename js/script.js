16d2704b6faff8c2c46e6a4f45504025fa0bc804
nordomecode <dumpyreg@gmail.com>
2023-08-30 14:29:36 +1000

    Add popular movies

--- js/script.js
+++ js/script.js
@@ -2,6 +2,49 @@ const global = {
     currentPage: window.location.pathname
 }
 
+//Fetch popular movies from DB
+async function displayPopularMovies() {
+    const {results} = await fetchAPIData('movie/popular');
+    
+    results.forEach(movie => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+        <a href="movie-details.html?id=${movie.id}">
+          ${
+            movie.poster_path
+                ? `<img
+                    src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+                    class="card-img-top"
+                    alt="${movie.title}"
+                    />`
+                : `<img
+                    src="../images/no-image.jpg"
+                    class="card-img-top"
+                    alt="${movie.title}"
+                    />`
+
+          }
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${movie.title}</h5>
+          <p class="card-text">
+            <small class="text-muted">Release: ${movie.release_date}</small>
+          </p>
+        </div>`
+
+        document.querySelector('#popular-movies').appendChild(div);
+    });
+    
+}
+
+async function fetchAPIData(endpoint) {
+    const API_KEY = '36fa2d5581d7f234871e95a33b4114cf';
+    const API_URL = 'https://api.themoviedb.org/3/';
+    const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+    const data = await response.json();
+    return data;
+}
 //Highlight active link
 function highlightActiveLink() {
     const links = document.querySelectorAll('.nav-link');
@@ -19,7 +62,7 @@ function init() {
     switch (global.currentPage) {
         case '/':
         case '/index.html':
-            console.log('Home');
+            displayPopularMovies();
             break;
         case '/shows.html':
             console.log('Shows');
